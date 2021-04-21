3df37b9cd265b125222e35593e11dc1b2b56b144
JacobM184 <jacobjm99@gmail.com>
2021-04-21 23:02:37 +1200

    Update frequency_relay_controller.c
    Replaced frequency_relay_controller.c with updated version

--- frequency_relay_controller.c
+++ frequency_relay_controller.c
@@ -12,12 +12,6 @@
 #include "io.h"
 #include "altera_avalon_pio_regs.h"
 
-//Task Priorities
-#define LED_UPDATE_TASK_PRIORITY				4
-#define SWITCH_POLL_TASK_PRIORITY				4
-#define FREQ_CALC_TASK_PRIORITY					4
-#define CHECK_STABILITY_TASK_PRIORITY			4
-#define LOAD_MANAGE_TASK_PRIORITY				4
 
 // Task State definitions
 #define MAINTAIN				2
@@ -29,231 +23,103 @@
 #define ROC_THRESHOLD 20
 #define FREQ_THRESHOLD 49
 
-//timer
-TimerHandle_t timer;
+QueueHandle_t Q_freq_calc;
+QueueHandle_t Q_freq_data;
 
 // Definition of Semaphore
-SemaphoreHandle_t led_sem;
+SemaphoreHandle_t mode_sem;
 
 // Local Function Prototypes
 int initOSDataStructs(void);
 int initCreateTasks(void);
 
-//Queue
-QueueHandle_t Q_freq_data;
-QueueHandle_t Q_stab_data;
-QueueHandle_t Q_stability;
+// Task Priorities
+#define FREQ_CALC_PRIORITY		3
+#define STABILITY_TASK_PRIORITY 4
+#define SWITCH_POLL_PRIORITY	1
 
-//Global Variables
-int modeSelect = MAINTAIN;
-double stab_data[2];
-
-uint8_t uiSwitchValue;
-uint8_t greenLedValue = 0x0;
-uint8_t stability;
-
-unsigned int LEDs;
-
-//function declarations
-void switch_poll_task(void *pvParameters);
-void led_update_task(void *pvParameters);
-void freq_calc_task(void *pvParameters);
-void load_manage_task(void *pvParameters);
-
-void handler_task(void *pvParameter);
-
-void freq_calc_task(void *pvParameter);
-void check_stability_task(void *pvParameter);
-
-void timer_task(void *pvParameter);
-void shed_loads();
-
-void reconnect_loads();
-
-
-void button_interrupt_isr()
-{
-
-	//xSemaphoreGiveFromISR(led_sem, 0);
-
-	if (modeSelect != MAINTAIN){
-		printf("Maintenance Mode\n");
-		modeSelect = MAINTAIN;
-		xTimerStopFromISR(timer,0);
-	} else {
-		modeSelect = STABLE;
-		printf("Stable Mode\n");
-		xTimerResetFromISR(timer,0);
-	}
-
-	// clears the edge capture register
-	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PUSH_BUTTON_BASE, 0x7);
-	return;
-}
-
-void shed_loads(){
-
-	uint8_t i;
-
-	for (i = 0x1; i < 0x1F; i*=2){
-		if(IORD_ALTERA_AVALON_PIO_DATA(SLIDE_SWITCH_BASE) & i){
-			IOWR_ALTERA_AVALON_PIO_DATA(RED_LEDS_BASE, (IORD_ALTERA_AVALON_PIO_DATA(SLIDE_SWITCH_BASE)& ~i));
-
-			greenLedValue = (greenLedValue || i);
-			IOWR_ALTERA_AVALON_PIO_DATA(GREEN_LEDS_BASE, greenLedValue);
-			printf("load shed");
-			return;
-		}
-	}
-}
-
-void reconnect_loads(){
-
-}
-
-void load_manage_task(void *pvParameters){
-
-	double rec_stability;
-	while(1){
-		while(uxQueueMessagesWaiting( Q_stability ) != 0){
-			xQueueReceive(Q_stability, &rec_stability,0);
-			printf("Rec Stab: %f\n",rec_stability);
-			if (rec_stability == 0){
-				shed_loads();
-			} else {
-				reconnect_loads();
-			}
-		}
-	}
-}
-
-void freq_calc_task(void *pvParameter){
-
-
-	double freq[100], dfreq[100];
-	int i = 99;
-
-	while(1){
-		if (modeSelect != MAINTAIN){
-			while(uxQueueMessagesWaiting( Q_freq_data ) != 0){
-				xQueueReceive(Q_freq_data,freq+i, 0);
-
-				if(i==0){
-					dfreq[0] = (freq[0]-freq[99]) * 2.0 * freq[0] * freq[99] / (freq[0]+freq[99]);
-				}
-				else{
-					dfreq[i] = (freq[i]-freq[i-1]) * 2.0 * freq[i]* freq[i-1] / (freq[i]+freq[i-1]);
-				}
-
-				if (dfreq[i] > 100.0){
-					dfreq[i] = 100.0;
-				}
-
-				stab_data[0] = freq[i];
-				stab_data[1] = dfreq[i];
-
-				xQueueSend(Q_stab_data,&stab_data,0);
-
-				i =	++i%100; //point to the next data (oldest) to be overwritten
-			}
-		}
-	}
-}
-void check_stability_task(void *pvParameter){
-	double rec_stabData[2];
-	double prevStability = stability;
-	while(1){
-		if (modeSelect != MAINTAIN){
-			while(uxQueueMessagesWaiting( Q_stab_data ) != 0){
-				xQueueReceive(Q_stab_data,&rec_stabData, 0);
-
-				double freq = rec_stabData[0];
-				double RoC = rec_stabData[1];
-
-				if (freq < FREQ_THRESHOLD || RoC > ROC_THRESHOLD){
-					stability = 0;
-					modeSelect = LOADMANAGE;
-//					printf("Sys unstable\n");
-				} else {
-					stability = 1;
-					//modeSelect = STABLE;
-//					printf("Sys stable\n");
-				}
-
-				if (prevStability != stability){
-					xTimerReset(timer,0);
-					printf("RESET\n");
-				}
-
-				prevStability = stability;
-			}
-		}
-	}
-}
-
-void switch_poll_task(void *pvParameter){
-    while(1){
-    	if (modeSelect != LOADMANAGE){
-    		uiSwitchValue = IORD_ALTERA_AVALON_PIO_DATA(SLIDE_SWITCH_BASE);
-			uiSwitchValue = uiSwitchValue & 0x1F;
-    	}
-    }
-}
-
-void led_update_task(void *pvParameter){
-    while(1){
-        if (modeSelect == MAINTAIN){
-            IOWR_ALTERA_AVALON_PIO_DATA(RED_LEDS_BASE, IORD_ALTERA_AVALON_PIO_DATA(SLIDE_SWITCH_BASE) & 0x1F);
-            IOWR_ALTERA_AVALON_PIO_DATA(GREEN_LEDS_BASE, 0x0);
-
-
-
-        }else if (modeSelect == LOADMANAGE){
-        	uiSwitchValue = (~IORD_ALTERA_AVALON_PIO_DATA(SLIDE_SWITCH_BASE) & 0x1F) & uiSwitchValue;
-        	IOWR_ALTERA_AVALON_PIO_DATA(RED_LEDS_BASE,uiSwitchValue);
-
-
-        }
-        vTaskDelay(100);
-    }
-}
+// Global Variables
+uint8_t stability = 1;
+uint8_t saveSwitch = 0x0;
+uint8_t switchState = 0x0;
 
 void freq_relay(){
 	#define SAMPLING_FREQ 16000.0
 	double temp = SAMPLING_FREQ/(double)IORD(FREQUENCY_ANALYSER_BASE, 0);
-
-	xQueueSendToBackFromISR( Q_freq_data, &temp, pdFALSE );
-
+	xQueueSendFromISR(Q_freq_calc, &temp, 0);
 	return;
 }
 
-void vTimerCallback(xTimerHandle t_timer){
-	double sysStability = stability;
-//	xQueueSend(Q_stability,&sysStability,0);
-	printf("tout: %f\n",sysStability);
+void freq_calc_task(void *pvParameter){
+	double freq[100], dfreq[100], freqData[2];
+	int i = 99;
 
+	while(1){
+		while(uxQueueMessagesWaiting(Q_freq_calc) != 0){
+			xQueueReceive(Q_freq_calc, &freq+i, 0);
+
+			if(i==0){
+				dfreq[0] = (freq[0]-freq[99]) * 2.0 * freq[0] * freq[99] / (freq[0]+freq[99]);
+			}
+			else{
+				dfreq[i] = (freq[i]-freq[i-1]) * 2.0 * freq[i]* freq[i-1] / (freq[i]+freq[i-1]);
+			}
+			freqData[0] = freq[i];
+			freqData[1] = dfreq[i];
+
+			xQueueSend(Q_freq_data,freqData,0);
+
+			i =	++i%100; //point to the next data (oldest) to be overwritten
+		}
+	}
+
+}
+
+void stability_task(void *pvParameter){
+
+	double freqData[2];
+	while(1){
+		while(uxQueueMessagesWaiting(Q_freq_calc) != 0){
+			xQueueReceive(Q_freq_data, freqData, 0);
+
+			if((freqData[0] < FREQ_THRESHOLD) || (freqData[1] > ROC_THRESHOLD)){
+				stability = 0;
+				saveSwitch = IORD_ALTERA_AVALON_PIO_DATA(SLIDE_SWITCH_BASE) & 0x1F;
+			}else{
+				stability = 1;
+			}
+
+		}
+
+	}
+}
+
+void switch_poll_task(void *pvParameter){
+	while(1){
+		switchState = IORD_ALTERA_AVALON_PIO_DATA(SLIDE_SWITCH_BASE) & 0x1F;
+	}
+}
+
+void led_control_task(void *pvParameter){
+	while(1){
+		if(uxSempahoreGetCount(mode_sem) == 1){
+			//load manange
+		}else{
+			//maintenance mode
+		}
+
+	}
 }
 
 int main(int argc, char* argv[], char* envp[])
 {
-
-	Q_freq_data = xQueueCreate( 100, sizeof(double) );
-	Q_stab_data = xQueueCreate( 2, sizeof(stab_data) );
-	Q_stability = xQueueCreate( 1, sizeof(double) );
+	Q_freq_calc = xQueueCreate(100, sizeof(double));
+	Q_freq_data = xQueueCreate(1, (sizeof(double)*2));
 
 	initOSDataStructs();
 	initCreateTasks();
 
-	IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PUSH_BUTTON_BASE, 0x7);
-	IOWR_ALTERA_AVALON_PIO_IRQ_MASK(PUSH_BUTTON_BASE, 0x4);
-	alt_irq_register(PUSH_BUTTON_IRQ, 0, button_interrupt_isr);
 	alt_irq_register(FREQUENCY_ANALYSER_IRQ, 0, freq_relay);
-
-	//Timer
-	timer = xTimerCreate("Load Shed Timer", pdMS_TO_TICKS(500), pdTRUE, NULL, vTimerCallback);
-	if (xTimerStart(timer, 0) != pdPASS){
-		printf("Cannot start timer");
-	}
 
 	vTaskStartScheduler();
 
@@ -267,18 +133,16 @@ int main(int argc, char* argv[], char* envp[])
 // This function simply creates a message queue and a semaphore
 int initOSDataStructs(void)
 {
-	led_sem = xSemaphoreCreateBinary();
+	mode_sem = xSemaphoreCreateBinary();
 	return 0;
 }
 
 // This function creates the tasks used in this example
 int initCreateTasks(void)
 {
+	xCreateTask(freq_calc_task, "freq_calc_task", TASK_STACKSIZE, NULL, FREQ_CALC_PRIORITY, NULL);
+	xCreateTask(stability_task, "stability_task", TASK_STACKSIZE, NULL, STABILITY_TASK_PRIORITY, NULL);
+	xCreateTask(switch_poll_task, "switch_poll_task", TASK_STACKSIZE, NULL, SWITCH_POLL_PRIORITY, NULL);
 
-	xTaskCreate(led_update_task, "led_update_task", TASK_STACKSIZE, NULL, LED_UPDATE_TASK_PRIORITY, NULL);
-	xTaskCreate(switch_poll_task, "switch_poll_task", TASK_STACKSIZE, NULL, SWITCH_POLL_TASK_PRIORITY, NULL);
-	xTaskCreate(check_stability_task, "check_stability_task", TASK_STACKSIZE, NULL, CHECK_STABILITY_TASK_PRIORITY, NULL);
-	xTaskCreate(freq_calc_task, "freq_calc_task", TASK_STACKSIZE, NULL, FREQ_CALC_TASK_PRIORITY, NULL);
-	xTaskCreate(load_manage_task, "load_manage_task", TASK_STACKSIZE, NULL, LOAD_MANAGE_TASK_PRIORITY, NULL);
 	return 0;
 }
