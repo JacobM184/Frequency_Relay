7e91bb1f591c16e55204ea1a9b5ea886dcd0d9b0
JacobM184 <jacobjm99@gmail.com>
2021-04-22 15:34:11 +1200

    Update frequency_relay_controller.c

--- frequency_relay_controller.c
+++ frequency_relay_controller.c
@@ -20,35 +20,33 @@
 // Definition of Task Stacks
 #define   TASK_STACKSIZE       2048
 
-#define ROC_THRESHOLD 20
+#define ROC_THRESHOLD 7
 #define FREQ_THRESHOLD 49
 
 QueueHandle_t Q_freq_calc;
 QueueHandle_t Q_freq_data;
-QueueHandle_t Q_timer_reset;
+QueueHandle_t Q_switch_state;
 
 TimerHandle_t timer500;
-TaskHandle_t Timer_Reset;
 
 // Definition of Semaphore
-SemaphoreHandle_t mode_sem;
+SemaphoreHandle_t stablephore;
 
 // Local Function Prototypes
 int initOSDataStructs(void);
 int initCreateTasks(void);
 
 // Task Priorities
-#define Timer_Reset_Task_P		(tskIDLE_PRIORITY+1)
-#define SWITCH_POLL_PRIORITY	1
-#define LED_CTRL_PRIORITY		2
-#define FREQ_CALC_PRIORITY		3
-#define STABILITY_TASK_PRIORITY 4
+#define SWITCH_POLL_PRIORITY		1
+#define LED_CTRL_PRIORITY			2
+#define FREQ_CALC_PRIORITY			3
+#define STABILITY_TASK_PRIORITY 	4
+#define LOAD_MANAGE_PRIORITY		5
 
 
 // Global Variables
 uint8_t stability = 1;
 uint8_t saveSwitch = 0x0;
-uint8_t switchState = 0x0;
 uint8_t prevStability = 1;
 
 void freq_relay(){
@@ -63,22 +61,22 @@ void freq_calc_task(void *pvParameter){
 	int i = 99;
 
 	while(1){
-		while(uxQueueMessagesWaiting(Q_freq_calc) != 0){
-			xQueueReceive(Q_freq_calc, &freq+i, 0);
+		xQueueReceive(Q_freq_calc, freq+i, portMAX_DELAY);
 
-			if(i==0){
-				dfreq[0] = (freq[0]-freq[99]) * 2.0 * freq[0] * freq[99] / (freq[0]+freq[99]);
-			}
-			else{
-				dfreq[i] = (freq[i]-freq[i-1]) * 2.0 * freq[i]* freq[i-1] / (freq[i]+freq[i-1]);
-			}
-			freqData[0] = freq[i];
-			freqData[1] = dfreq[i];
-
-			xQueueSend(Q_freq_data,freqData,0);
-
-			i =	++i%100; //point to the next data (oldest) to be overwritten
+		if(i==0){
+			dfreq[0] = (freq[0]-freq[99]) * 2.0 * freq[0] * freq[99] / (freq[0]+freq[99]);
 		}
+		else{
+			dfreq[i] = (freq[i]-freq[i-1]) * 2.0 * freq[i]* freq[i-1] / (freq[i]+freq[i-1]);
+		}
+		freqData[0] = freq[i];
+		freqData[1] = dfreq[i];
+
+//			printf("Freq: %f\n",freq[i]);
+		xQueueSend(Q_freq_data,freqData,0);
+
+		i =	++i%100; //point to the next data (oldest) to be overwritten
+
 	}
 
 }
@@ -87,72 +85,68 @@ void stability_task(void *pvParameter){
 
 	double freqData[2];
 	while(1){
-		while(uxQueueMessagesWaiting(Q_freq_calc) != 0){
-			xQueueReceive(Q_freq_data, freqData, 0);
+		xQueueReceive(Q_freq_data, freqData, portMAX_DELAY);
 
-			if((freqData[0] < FREQ_THRESHOLD) || (freqData[1] > ROC_THRESHOLD)){
-				stability = 0;
-				saveSwitch = IORD_ALTERA_AVALON_PIO_DATA(SLIDE_SWITCH_BASE) & 0x1F;
-			}else{
-				stability = 1;
-			}
-
-			if(stability != prevStability){
-				xQueueSend(Q_timer_reset, &stability, 0);
-				prevStability = stability;
-			}
-
+		if((freqData[0] < FREQ_THRESHOLD) || (freqData[1] > ROC_THRESHOLD)){
+			stability = 0;
+			saveSwitch = IORD_ALTERA_AVALON_PIO_DATA(SLIDE_SWITCH_BASE) & 0x1F;
+		}else{
+			stability = 1;
 		}
 
-		
+//		printf("Stab: %d\n",stability);
+		if(stability != prevStability){
 
+			prevStability = stability;
+		}
 	}
 }
 
 void switch_poll_task(void *pvParameter){
+	uint8_t prevSwitchState = 0x0;
 	while(1){
-		switchState = IORD_ALTERA_AVALON_PIO_DATA(SLIDE_SWITCH_BASE) & 0x1F;
+
+		uint8_t switchState = IORD_ALTERA_AVALON_PIO_DATA(SLIDE_SWITCH_BASE) & 0x1F;
+
+		if (prevSwitchState != switchState){
+			xQueueSend(Q_switch_state, &switchState,0);
+		}
+		prevSwitchState = switchState;
+
+
 	}
 }
 
 void led_control_task(void *pvParameter){
+	uint8_t rec_switchState;
 	while(1){
-		if(uxSempahoreGetCount(mode_sem) == 1){
-			//load manange LED 
-		}else{
-			//maintenance mode LED
-		}
-
+		xQueueReceive(Q_switch_state,&rec_switchState,portMAX_DELAY);
+		IOWR_ALTERA_AVALON_PIO_DATA(RED_LEDS_BASE,rec_switchState);
 	}
 }
 
 void load_manage_task(void *pvParameter){
 	while(1){
+		xSemaphoreTake(stablephore,portMAX_DELAY);
 
-	}
-}
-
-void Timer_Reset_Task(void *pvParameters ){ //reset timer if any of the push button is pressed
-	uint8_t temp;
-	while(1){
-		if ((uxQueueMessagesWaiting(Q_timer_reset) != 0)){
-			xQueueReceive(Q_timer_reset, &temp, 0);
-			xTimerReset(timer500, 0);
+		if (stability == 0){
+			printf("Unstable\n");
+		} else {
+			printf("Stable\n");
 		}
-
 	}
 }
 
 
-void vTimerCallback(xTimerHandle t_timer){ //Timer flashes green LEDs
-	
+
+void vTimerCallback(xTimerHandle t_timer){
+	xSemaphoreGive(stablephore);
 }
 
 int main(int argc, char* argv[], char* envp[])
 {
-	Q_freq_calc = xQueueCreate(100, sizeof(double));
-	Q_freq_data = xQueueCreate(1, (sizeof(double)*2));
-	Q_timer_reset = xQueueCreate(1, sizeof(uint8_t));
+
+
 
 	initOSDataStructs();
 	initCreateTasks();
@@ -160,6 +154,7 @@ int main(int argc, char* argv[], char* envp[])
 	alt_irq_register(FREQUENCY_ANALYSER_IRQ, 0, freq_relay);
 
 	timer500 = xTimerCreate("Timer500", pdMS_TO_TICKS(500), pdTRUE, NULL, vTimerCallback);
+	xTimerStart(timer500,0);
 
 	vTaskStartScheduler();
 
@@ -173,7 +168,10 @@ int main(int argc, char* argv[], char* envp[])
 // This function simply creates a message queue and a semaphore
 int initOSDataStructs(void)
 {
-	mode_sem = xSemaphoreCreateBinary();
+	Q_freq_calc = xQueueCreate(100, sizeof(double));
+	Q_freq_data = xQueueCreate(10, (sizeof(double)*2));
+	Q_switch_state = xQueueCreate(10, sizeof(uint8_t));
+	stablephore = xSemaphoreCreateBinary();
 	return 0;
 }
 
@@ -182,9 +180,10 @@ int initCreateTasks(void)
 {
 	xTaskCreate(freq_calc_task, "freq_calc_task", TASK_STACKSIZE, NULL, FREQ_CALC_PRIORITY, NULL);
 	xTaskCreate(stability_task, "stability_task", TASK_STACKSIZE, NULL, STABILITY_TASK_PRIORITY, NULL);
+	xTaskCreate(load_manage_task, "load_manage_task", TASK_STACKSIZE, NULL, LOAD_MANAGE_PRIORITY, NULL);
+
 	xTaskCreate(switch_poll_task, "switch_poll_task", TASK_STACKSIZE, NULL, SWITCH_POLL_PRIORITY, NULL);
 	xTaskCreate(led_control_task, "led_control_task", TASK_STACKSIZE, NULL, LED_CTRL_PRIORITY, NULL);
-	xTaskCreate( Timer_Reset_Task, "0", configMINIMAL_STACK_SIZE, NULL, Timer_Reset_Task_P, &Timer_Reset );
 
 	return 0;
 }
